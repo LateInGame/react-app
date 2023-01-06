fd3cbcebcd8f083412f37a4826b111cd0dfd1534
David Deus <davidafonsodeus@outlook.com>
2023-01-06 15:48:38 +0000

    header and banner img updated

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -47,7 +47,7 @@ export const Banner = () => {
         <Row className="align-items-center">
           <Col xs={12} md={6} xl={7}>
             <span className="tagline">Welcome to my Portfolio</span>
-            <h1>{" Hi I'm David Cortez "}<span className="wrap">{text}</span></h1>
+            <h1>{" Hi!! I'm David Cortez "}<span className="wrap">{text}</span></h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             <button onClick={() => console.log('connect')}>Let's connect<ArrowRightCircle size={25}/></button>
           </Col>
